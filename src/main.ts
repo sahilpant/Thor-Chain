6f749cb566543548ac6cc1a3cc0854eeafcbd869
sahil pant <sahilpant16@gmail.com>
2020-10-22 13:16:48 +0530

    fixed bug in validation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,7 @@ async function bootstrap() {
   (global as any).WebSocket = require('ws');
   (global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
